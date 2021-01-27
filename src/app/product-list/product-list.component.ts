32e40c70eead7314a17c66ee371ce34a6177e232
thakgatso1 <thakgatsochuene1@gmail.com>
2021-01-27 12:11:04 +0200

    product-list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,7 +8,7 @@ import { Product } from '../product';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-products: product[];
+products: Product[];
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
